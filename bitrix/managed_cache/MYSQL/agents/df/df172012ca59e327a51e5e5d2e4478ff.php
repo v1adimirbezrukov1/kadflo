941058f411fa49b9fd417ae4f83784dc275954ee
NastArtWeb <v.bezrukov@nastart-web.ru>
2025-05-12 13:41:04 +0000

    Добавлены все файлы

--- bitrix/managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
+++ bitrix/managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001747055401';
-$dateexpire = '001747059001';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1747055402;}';
+$datecreate = '001747056961';
+$dateexpire = '001747060561';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1747056962;}';
 return true;
 ?>
