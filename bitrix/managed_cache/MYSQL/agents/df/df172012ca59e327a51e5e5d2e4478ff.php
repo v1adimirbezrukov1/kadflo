9c09c7b71b9417ef8925c9a4f2a567f6f576c333
NastArtWeb <v.bezrukov@nastart-web.ru>
2025-05-13 06:24:59 +0000

    remove folder

--- bitrix/managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
+++ bitrix/managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001747117081';
-$dateexpire = '001747120681';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1747117082;}';
+$datecreate = '001747117441';
+$dateexpire = '001747121041';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1747117442;}';
 return true;
 ?>
